8315f4da8142fcf75549eb7d4c76b527c31eb2af
adiujjaman-emon <emon94969@gmail.com>
2025-08-23 03:22:15 +0600

    Login Screen

---
+++ lib/views/screens/authentication_screens/login_screen.dart
@@ -0,0 +1,184 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget{
+  const LoginScreen({super.key});
+
+@override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor:Colors.white.withOpacity(
+        0.95,
+        ),
+      body:Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Login To Your Account",
+                style: GoogleFonts.getFont(
+                'Lato',
+                color:Color(0xFF0d120E),
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0.2,
+                fontSize: 23,
+                ),
+                ),
+                 Text(
+                  "To Explore The App",
+                  style: GoogleFonts.getFont(
+                    'Lato',
+                    color: Color(0xFF0d120E),
+                    fontSize: 14,
+                    letterSpacing: 0.2,
+                  ),
+                  ),
+                 Image.asset(
+                    'assets/images/illustration.png',width: 200,height:200,
+                    ),
+        
+                 Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Email',
+                    style: GoogleFonts.getFont(
+                      'Nunito Sans',
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 0.2,
+                    ),
+                    ),
+                 ),
+        
+                 TextFormField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(9),
+                    ),
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    labelText: 'Enter Your Mail',
+                    labelStyle: GoogleFonts.getFont(
+                      'Nunito Sans',
+                      fontSize: 14,
+                      letterSpacing: 0.1,
+                    ),
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Image.asset(
+                        'assets/icons/email.png',
+                        width: 10,
+                        height: 20,
+                      ),
+                    )
+                  ),
+                 ),
+        
+                 SizedBox(
+                  height: 20,
+                 ),
+
+                 Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Password',
+                    style: GoogleFonts.getFont(
+                      'Nunito Sans',
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 0.2,
+                    ),
+                    ),
+                 ),
+
+                 TextFormField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(9),
+                    ),
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    labelText: 'Enter Your Password',
+                    labelStyle: GoogleFonts.getFont(
+                      'Nunito Sans',
+                      fontSize: 14,
+                      letterSpacing: 0.1,
+                    ),
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Image.asset(
+                        'assets/icons/password.png',
+                        width: 10,
+                        height: 20,
+                      ),
+                    ),
+        
+                    suffixIcon: Icon(Icons.visibility),
+                  ),
+                 ),
+
+                 SizedBox(
+                  height: 20,
+                 ),
+
+                 Container(
+                  width: 319,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(45),
+                    gradient: LinearGradient(colors:[
+                      Color(0xFF102DE1),
+                      Color(0xCC0D6EFF),
+                      ],
+                      ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Sign In',
+                       style: GoogleFonts.getFont(
+                      'Lato',
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                   ),
+                  ),
+                 ),
+
+                 SizedBox(
+                  height: 20,
+                 ),
+
+
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Need an account?',
+                       style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1,
+                      ),
+                      ),
+
+                    Text(
+                      'Sign Up',
+                      style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.w900,
+                      color: Colors.blueAccent,
+                      ),
+                    ),
+                    ],
+                 ),
+
+        
+            ],
+          ),
+        ),
+      ),
+    );
+    
+  }
+}
